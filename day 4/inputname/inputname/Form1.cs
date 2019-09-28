7806a20d7e5917212a05cefb69263a90a7811113
ayon749 <mushfiqurrahman749@gmail.com>
2019-09-28 10:00:18 +0600

    committed

---
+++ day 4/inputname/inputname/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inputname
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+          string name=  nameInputbox.Text;
+          MessageBox.Show("Hi " + name + " welcom!!!");
+        }
+    }
+}
