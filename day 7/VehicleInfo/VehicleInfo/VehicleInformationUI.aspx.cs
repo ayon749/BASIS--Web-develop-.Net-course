6da145b7dfda7400bf348ef23d443bd3b56e7a90
ayon749 <mushfiqurrahman749@gmail.com>
2019-10-10 22:17:29 +0600

    New file Added

---
+++ day 7/VehicleInfo/VehicleInfo/VehicleInformationUI.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VehicleInfo
+{
+	public partial class VehicleInformationUI : System.Web.UI.Page
+	{
+		Vehicle aVehicle = new Vehicle();
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void createButton_Click(object sender, EventArgs e)
+		{
+			aVehicle.VehicleName = vehicleNameTextBox.Text;
+			aVehicle.RegNo = regNoTextBox.Text;
+			ViewState["AVehicle"] = aVehicle;
+			vehicleNameTextBox.Text = string.Empty;
+			regNoTextBox.Text = string.Empty;
+			
+
+		}
+
+		protected void speedEnterButton_Click(object sender, EventArgs e)
+		{
+			Vehicle tempVehicle =( Vehicle) ViewState["AVehicle"];
+			tempVehicle.Speed.Add(Convert.ToDouble(speedTextBox.Text));
+
+
+
+			ViewState["AVehicle"] =tempVehicle;
+			speedTextBox.Text = string.Empty;
+
+
+		}
+
+		protected void showButton_Click(object sender, EventArgs e)
+		{
+			Vehicle bVehicle = (Vehicle)ViewState["AVehicle"];
+
+			double maxSpeed = bVehicle.GetMaxSpeed();
+			double minSpeed = bVehicle.GetMMinSpeed();
+			double avgSpeed = bVehicle.GetAvgSpeed();
+			maxSpeedLabel.Text = maxSpeed.ToString();
+			minSpeedLabel.Text = minSpeed.ToString();
+			avgSpeedLabel.Text = avgSpeed.ToString();
+
+
+		}
+	}
+}
---
+++ day 8/VehicleInfo/VehicleInfo/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleInfo
+{
+	[Serializable]
+	public class Vehicle
+	{
+		public string VehicleName, RegNo;
+		public List<double> Speed=new List<double>();
+
+		public double GetMaxSpeed()
+		{
+			double max = Speed.Max();
+			return max;
+		}
+		public double GetMMinSpeed()
+		{
+			double min = Speed.Min();
+			return min;
+		}
+		public double GetAvgSpeed()
+		{
+		
+			double avg = Speed.Average();
+			return avg;
+		}
+
+
+
+
+	}
+}
